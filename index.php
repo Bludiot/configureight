cf1909817fae7bb60d0d77e788ca2fbaf791376b
ControlledChaos <greg@ccdzine.com>
2023-09-03 12:52:41 -0700

    Use minified stylesheet. Clean up PHP.

--- index.php
+++ index.php
@@ -4,36 +4,38 @@
 	<meta charset="UTF-8">
 
 	<!-- Dynamic title tag -->
-	<?php echo Theme::metaTags('title'); ?>
+	<?php echo Theme :: metaTags( 'title' ); ?>
 
 	<!-- Dynamic description tag -->
-	<?php echo Theme::metaTags('description'); ?>
+	<?php echo Theme :: metaTags( 'description' ); ?>
 
 	<!-- Include CSS Styles from this theme -->
-	<?php echo Theme::css('css/style.css'); ?>
+	<?php echo Theme :: css( 'css/style.min.css' ); ?>
 
 	<!-- Load plugins with the hook siteHead -->
-	<?php Theme::plugins('siteHead') ?>
+	<?php Theme :: plugins( 'siteHead' ); ?>
 </head>
 <body>
 	<!-- Load plugins with the hook siteBodyBegin -->
-	<?php Theme::plugins('siteBodyBegin') ?>
+	<?php Theme :: plugins( 'siteBodyBegin' ); ?>
 
-	<img src="<?php echo $site->logo() ?>" alt="" width="128">
-	<h1><?php echo $site->title() ?></h1>
-	<h2><?php echo $site->slogan() ?></h2>
+	<?php if ( ! empty( $site->logo() ) ) : ?>
+	<img src="<?php echo $site->logo(); ?>" alt="<?php echo $site->title(); ?>" width="128">
+	<?php endif ?>
 
-	<?php if ($WHERE_AM_I=='page'): ?>
+	<h1><?php echo $site->title(); ?></h1>
+	<h2><?php echo $site->slogan(); ?></h2>
+
+	<?php if ( $WHERE_AM_I == 'page' ) : ?>
 		<h3><?php echo $page->title(); ?></h3>
 
-	<?php elseif ($WHERE_AM_I=='home'): ?>
-		<?php foreach ($content as $page): ?>
+	<?php elseif ( $WHERE_AM_I == 'home' ) : ?>
+		<?php foreach ( $content as $page ) : ?>
 		<h3><?php echo $page->title(); ?></h3>
 		<?php endforeach ?>
-
 	<?php endif ?>
 
 	<!-- Load plugins with the hook siteBodyBegin -->
-	<?php Theme::plugins('siteBodyEnd') ?>
+	<?php Theme :: plugins( 'siteBodyEnd' ); ?>
 </body>
 </html>
