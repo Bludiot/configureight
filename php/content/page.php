5c4cb6927ffec98d14a509cad8d7d4bd69d508d7
ControlledChaos <greg@ccdzine.com>
2023-09-03 18:45:34 -0700

    Page template markup.

--- php/content/page.php
+++ php/content/page.php
@@ -9,8 +9,29 @@
  */
 
 ?>
-<main class="site-main">
+<article class="site-article" role="article">
 
-	<h1><?php echo $page->title(); ?></h1>
+	<?php Theme :: plugins( 'pageBegin' ); ?>
 
-</main>
+	<header class="page-header">
+		<h1><?php echo $page->title(); ?></h1>
+
+		<?php if ( $page->description() ) {
+			printf(
+				'<p class="page-description">%s</p>',
+				$page->description()
+			);
+		} ?>
+	</header>
+
+	<div class="page-content" itemprop="articleBody">
+		<?php echo $page->contentBreak(); ?>
+	</div>
+
+	<?php if ( $page->readMore() ) : ?>
+		<p><a class="button" href="<?php echo $page->permalink(); ?>" role="button"><?php echo $L->get( 'Read More' ); ?></a></p>
+	<?php endif; ?>
+
+	<?php Theme :: plugins( 'pageEnd' ); ?>
+
+</article>
