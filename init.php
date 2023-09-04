daac3e02d6c53c161fbeb34f6b48775a7b9593d5
ControlledChaos <greg@ccdzine.com>
2023-09-03 17:23:08 -0700

    Add init file.

---
+++ init.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Initialize theme
+ *
+ * @package    BS Bludit
+ * @subpackage Functions
+ * @since      1.0.0
+ */
+
+// Stop if accessed directly.
+if ( ! defined( 'BLUDIT' ) ) {
+  die( 'You are not allowed to access this file.' );
+}
+
+// For later plugin development.
+// if ( $themePlugin == false ) {}
