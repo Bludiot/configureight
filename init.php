2509d7648da8e67d066963a0f550bebaec1c008e
ControlledChaos <greg@ccdzine.com>
2023-09-06 16:39:07 -0700

    Docblock edits.

--- init.php
+++ init.php
@@ -34,29 +34,6 @@ function user_logged_in() {
 }
 
 /**
- * Page description
- *
- * Gets the page description or
- * an excerpt of the content.
- *
- * @since  1.0.0
- * @global object $page
- * @return string Returns the description.
- */
-function page_description() {
-
-	global $page;
-
-	if ( $page->description() ) {
-		$page_desc = $page->description();
-	} else {
-		$page_desc  = substr( strip_tags( $page->content() ), 0, 85 );
-		$page_desc .= '&hellip;';
-	}
-	return $page_desc;
-}
-
-/**
  * Is RTL language
  *
  * @since  1.0.0
@@ -85,55 +62,6 @@ function is_rtl() {
 }
 
 /**
- * Body classes
- *
- * For the class attribute on the `<body>` element.
- *
- * @since  1.0.0
- * @return string Returns a string of classes.
- */
-function body_classes() {
-
-	global $site, $url, $page;
-
-	$classes = [];
-
-	if ( is_rtl() ) {
-		$classes[] = 'rtl';
-	} else {
-		$classes[] = 'ltr';
-	}
-
-	if ( user_logged_in() ) {
-		$classes[] = 'user-logged-in';
-	} else {
-		$classes[] = 'user-logged-out';
-	}
-
-	if ( 'home' == $url->whereAmI() ) {
-		$classes[] = 'home';
-
-		if ( ! $site->homepage() ) {
-			$classes[] = 'blog';
-		}
-	}
-
-	if ( 'blog' == $url->whereAmI() ) {
-		$classes[] = 'blog';
-	}
-
-	if ( 'page' == $url->whereAmI() ) {
-		if ( $page->isStatic() ) {
-			$classes[] = 'page';
-		} else {
-			$classes[] = 'post';
-		}
-	}
-
-	return implode( ' ', $classes );
-}
-
-/**
  * Asset file suffix
  *
  * Gets minified file if not in debug mode.
@@ -152,4 +80,92 @@ function asset_min() {
 		return '';
 	}
 	return '.min';
+}
+
+/**
+ * Body classes
+ *
+ * For the class attribute on the `<body>` element.
+ *
+ * @since  1.0.0
+ * @global object $site Site class.
+ * @global object $url  Url class.
+ * @global object $page Page class.
+ * @return string Returns a string of classes.
+ */
+function body_classes() {
+
+	// Get global variables.
+	global $site, $url, $page;
+
+	// Set up classes.
+	$classes = [];
+
+	// Language direction.
+	if ( is_rtl() ) {
+		$classes[] = 'rtl';
+	} else {
+		$classes[] = 'ltr';
+	}
+
+	// User logged in/out.
+	if ( user_logged_in() ) {
+		$classes[] = 'user-logged-in';
+	} else {
+		$classes[] = 'user-logged-out';
+	}
+
+	// Home page.
+	if ( 'home' == $url->whereAmI() ) {
+		$classes[] = 'home';
+
+		// If home is not static.
+		if ( ! $site->homepage() ) {
+			$classes[] = 'blog';
+		}
+	}
+
+	// If blog, not home.
+	if ( 'blog' == $url->whereAmI() ) {
+		$classes[] = 'blog';
+	}
+
+	// If singular content.
+	if ( 'page' == $url->whereAmI() ) {
+
+		// If static content.
+		if ( $page->isStatic() ) {
+			$classes[] = 'page';
+
+		// If not static content.
+		} else {
+			$classes[] = 'post';
+		}
+	}
+
+	// Return a string of space-separated classes.
+	return implode( ' ', $classes );
+}
+
+/**
+ * Page description
+ *
+ * Gets the page description or
+ * an excerpt of the content.
+ *
+ * @since  1.0.0
+ * @global object $page
+ * @return string Returns the description.
+ */
+function page_description() {
+
+	global $page;
+
+	if ( $page->description() ) {
+		$page_desc = $page->description();
+	} else {
+		$page_desc  = substr( strip_tags( $page->content() ), 0, 85 );
+		$page_desc .= '&hellip;';
+	}
+	return $page_desc;
 }
