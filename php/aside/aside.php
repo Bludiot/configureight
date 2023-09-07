87f114ea2d4b4127e6e35fd859507fab07bb8b65
ControlledChaos <greg@ccdzine.com>
2023-09-06 17:28:42 -0700

    Add aside/sidebar files.

---
+++ php/aside/aside.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Aside/sidebar content template
+ *
+ * @package    BS Bludit
+ * @subpackage Templates
+ * @category   Aside
+ * @since      1.0.0
+ */
+
+?>
+<aside class="site-sidebar wrapper-general"></aside>
