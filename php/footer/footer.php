ba42203071478850189ba29d991d273c3b3da618
ControlledChaos <greg@ccdzine.com>
2023-09-06 19:27:32 -0700

    Add Schema to copyright message.

--- php/footer/footer.php
+++ php/footer/footer.php
@@ -8,9 +8,16 @@
  * @since      1.0.0
  */
 
+$copyright = sprintf(
+	'<p class="copyright" itemscope="itemscope" itemtype="http://schema.org/CreativeWork">&copy; <span itemprop="copyrightYear">%s</span> <span itemprop="copyrightHolder">%s.</span> %s</p>',
+	date( 'Y' ),
+	$site->title(),
+	$L->get( 'copyright-message' )
+);
+
 ?>
 <footer class="site-footer">
 	<div class="wrapper-general">
-		<p class="copyright">&copy;<?php echo date( 'Y' ) . ' ' . $site->title() . ' | '; $L->p( 'copyright-message' ); ?></p>
+		<?php echo $copyright; ?>
 	</div>
 </footer>
