26bff3075740e023031b7694b1f2873841fad9eb
ControlledChaos <greg@ccdzine.com>
2023-09-07 08:13:13 -0700

    Rework blog layout markup & CSS.

--- php/content/home.php
+++ php/content/home.php
@@ -22,30 +22,33 @@ use function BS_Init\page_description;
 
 		<?php Theme :: plugins( 'pageBegin' ); ?>
 
-		<header class="page-header">
-			<h2><a href="<?php echo $page->permalink(); ?>"><?php echo $page->title(); ?></a></h2>
-			<?php echo page_description(); ?>
-		</header>
+		<div class="blog-wrap">
+			<?php if ( $page->coverImage() ) : ?>
+			<figure class="page-cover page-cover-home">
+				<a href="<?php echo $page->permalink(); ?>">
+					<img src="<?php echo $page->coverImage(); ?>" />
+				</a>
+				<figcaption class="screen-reader-text"><?php echo $page->title(); ?></figcaption>
+			</figure>
+			<?php endif ?>
 
-		<?php if ( $page->coverImage() ) : ?>
-		<figure class="page-cover page-cover-home">
-			<a href="<?php echo $page->permalink(); ?>">
-				<img src="<?php echo $page->coverImage(); ?>" width="240" />
-			</a>
-			<figcaption class="screen-reader-text"><?php echo $page->title(); ?></figcaption>
-		</figure>
-		<?php endif ?>
+			<div class="page-summary">
 
-		<footer class="page-info">
-			<p>
-			<span class="page-info-entry"><span class="bi bi-calendar"></span> <?php echo $page->date(); ?></span>
-			<span class="page-info-entry"><span class="bi bi-clock-history"></span> <?php echo $L->get( 'Reading time' ) . ': ' . $page->readingTime(); ?></span>
-			</p>
-		</footer>
+				<header class="page-header">
+					<h2><a href="<?php echo $page->permalink(); ?>"><?php echo $page->title(); ?></a></h2>
+				</header>
 
-		<?php if ( $page->readMore() ) : ?>
-			<p><a class="button" href="<?php echo $page->permalink(); ?>" role="button"><?php echo $L->get( 'Read More' ); ?></a></p>
-		<?php endif; ?>
+				<?php echo page_description(); ?>
+
+				<footer class="page-info">
+					<p>
+						<span class="page-info-entry"><span class="bi bi-calendar"></span> <?php echo $page->date(); ?></span>
+						<br />
+						<span class="page-info-entry"><span class="bi bi-clock-history"></span> <?php echo $L->get( 'Reading time' ) . ': ' . $page->readingTime(); ?></span>
+					</p>
+				</footer>
+			</div>
+		</div>
 
 		<?php Theme :: plugins( 'pageEnd' ); ?>
 	<?php endforeach; ?>
