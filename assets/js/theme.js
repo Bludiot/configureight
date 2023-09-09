b56af7f62f78b275d65a5b5186df06525d3ca516
ControlledChaos <greg@ccdzine.com>
2023-09-09 15:36:13 -0700

    Add loaded class to body w/ demo CSS.

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -1,3 +1,10 @@
 /**
  * Theme scripts
  */
+
+// Add body class when document is ready.
+( function ($) {
+	$(document).ready( function() {
+		$( 'body' ).addClass( 'page-loaded' );
+	});
+})(jQuery);
