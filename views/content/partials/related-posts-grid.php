48643a05f738857f20993e8fec4db44d224d5c7f
ControlledChaos <greg@ccdzine.com>
2023-11-28 13:55:19 -0800

    Related posts: grid layout.

--- views/content/partials/related-posts-grid.php
+++ views/content/partials/related-posts-grid.php
@@ -13,6 +13,10 @@ use function CFE_Func\{
 	theme,
 	get_related
 };
+use function CFE_Tags\{
+	page_description,
+	icon
+};
 
 $get_related = get_related();
 
@@ -20,6 +24,15 @@ $get_related = get_related();
 $heading = $L->get( 'Related Posts' );
 if ( ! empty( theme()->related_heading() ) ) {
 	$heading = theme()->related_heading();
+}
+
+// Maximum posts class.
+$max = 'max-related-3';
+if ( 3 != theme()->max_related() ) {
+	$max = sprintf(
+		'max-related-%s',
+		theme()->max_related()
+	);
 }
 
 ?>
@@ -32,29 +45,23 @@ if ( ! empty( theme()->related_heading() ) ) {
 		theme()->related_heading_el()
 	); ?>
 
-	<div class="related-loop related-style-<?php echo theme()->related_style(); ?>">
+	<div class="related-loop related-style-<?php echo theme()->related_style(); ?> <?php echo $max; ?>">
 		<?php foreach ( $get_related as $related ) : ?>
-		<article class="post item">
-			<?php if ( $related->coverImage() ) : ?>
-			<figure>
-				<img src="<?php echo $related->thumbCoverImage(); ?>" alt="">
-			</figure>
-			<?php endif; ?>
-			<div class="post-inner-content">
-				<a href="<?php echo $related->permalink(); ?>" class="post-title">
-					<?php echo $related->title(); ?>
-				</a>
-			</div>
-			<div class="post-meta">
-				<time datetime="<?php echo $related->dateRaw( 'c' ); ?>">
-					<?php echo $related->date(); ?>
-				</time>
-				<span class="tags">
-					<a href="<?php echo DOMAIN_CATEGORIES . $related->categoryKey(); ?>" rel="tag">
-						<?php echo $related->category(); ?>
-					</a>
-				</span>
-			</div>
+		<article class="related-post">
+			<a href="<?php echo $related->permalink(); ?>">
+				<?php if ( $related->coverImage() ) : ?>
+				<figure class="related-cover">
+					<img src="<?php echo $related->thumbCoverImage(); ?>" alt="">
+				</figure>
+				<?php endif; ?>
+				<div class="related-content">
+					<p class="related-title">
+						<?php echo $related->title(); ?>
+					</p>
+					<?php echo icon( 'dots-h', true, 'related-icon-read' ); ?>
+					<p class="related-description"><?php echo page_description( $related->key() ); ?></p>
+				</div>
+			</a>
 		</article>
 		<?php endforeach; ?>
 	</div>
