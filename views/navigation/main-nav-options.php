87b72d369fd14892393fd996cb64df0fe3fe1a94
ControlledChaos <greg@ccdzine.com>
2025-04-14 08:49:05 -0700

    Fix usort warning.

--- views/navigation/main-nav-options.php
+++ views/navigation/main-nav-options.php
@@ -127,7 +127,7 @@ use function CFE_Tags\{
 
 				// Sort by position.
 				usort( $children, function( $a, $b ) {
-					return $a->position() > $b->position();
+					return $a->position() <=> $b->position();
 				} );
 
 				$sub_menu = '<ul class="nav-list main-nav-sub-list">';
